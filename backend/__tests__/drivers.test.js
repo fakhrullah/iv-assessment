91d3671f783bc61dd2c73c65ddd87116e0c41246
Fakhrullah Padzil <fakhrullah@fajarhac.com>
2021-10-05 16:42:14 +0800

    Implement test when cars count is specified

--- backend/__tests__/drivers.test.js
+++ backend/__tests__/drivers.test.js
@@ -4,12 +4,17 @@ const BASE_URL = 'http://localhost:30350'
 
 test('Should return 200 success with array of drivers', async () => {
   const resp = await axios.get(`${BASE_URL}/drivers?latitude=1.285194&longitude=103.8522982`);
-  console.log(resp);
+
   expect(resp.status).toBe(200);
   expect(resp.data.drivers.length).toBeGreaterThanOrEqual(1);
 })
 
-test.todo('Should return success with 4 drivers only',)
+test('Should return success with 4 drivers only', async () => {
+  const resp = await axios.get(`${BASE_URL}/drivers?latitude=1.285194&longitude=103.8522982&count=4`);
+
+  expect(resp.status).toEqual(200);
+  expect(resp.data.drivers.length).toEqual(4);
+})
 
 test.todo('Should return error missing latitude parameter',)
 
