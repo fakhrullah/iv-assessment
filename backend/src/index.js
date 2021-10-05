6a3874197dab3b18f269d422a9b34891f496ec78
Fakhrullah Padzil <fakhrullah@fajarhac.com>
2021-10-05 18:11:41 +0800

    Implement longitude error handling

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,15 @@ fastify.get('/drivers', async (request, reply) => {
   // Latitude & Longitude is required
   if (latitude === undefined) {
     errorMessage = 'Missing latitude. Latitude is required.';
+
+    if (longitude === undefined) {
+      errorMessage = 'Missing latitude & longitude. Both latitude & longitude is required.'
+    }
   }
+  if (longitude === undefined) {
+    errorMessage = 'Missing longitude. Longitude is required.'
+  }
+
 
   const resp = await axios.get(`${externalAPI}`, {
     params: {
