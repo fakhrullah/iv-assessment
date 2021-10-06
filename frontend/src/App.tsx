4921194cf9e9baf4b1041774b027ca2c605dba0e
Fakhrullah Padzil <fakhrullah@fajarhac.com>
2021-10-06 12:24:52 +0800

    Add google map & set center base selected office
    Use Google Map because for the time being it is the best Map service.
    But it will be expensive on large scale.
    
    Use google-react-map package because it the best I can find.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import GoogleMapReact from 'google-map-react';
+
+interface LocationModel {
+  lat: number
+  lng: number
+}
+
+interface OfficeModel {
+  name: string
+  slug: string
+  location: LocationModel
+}
 
 function App() {
 
-  const offices = ['Singapore', 'London'];
+  const offices: OfficeModel[] = [
+    {
+      name: 'Singapore',
+      slug: 'singapore',
+      location: {
+        lat: 1.285194,
+        lng: 103.8522982
+      }
+    },
+    {
+      name: 'London',
+      slug: 'london',
+      location: {
+        lat: 51.5049375,
+        lng: -0.0964509
+      }
+    },
+  ];
+
+  const getOfficeLocation = (slug: string): LocationModel => offices
+    .find((office) => office.slug === slug)?.location || {lat: 0, lng: 0};
+
+  const [office, setOffice] = useState<string>('singapore');
+  const zoom = 8;
 
   return (
     <div className="App">
       <div>
         Choose location:
-        <select>
-          {offices.map((office) => (<option value={office}>{office}</option>))}
+        <select
+          onChange={(e) => setOffice(e.target.value)}
+          >
+          {
+            offices
+              .map((office) => (
+                <option 
+                key={office.slug} 
+                value={office.slug}
+                >{office.name}</option>
+              ))
+          }
         </select>
       </div>
 
       <div>Slider car number</div>
 
-      <div style={{width: '100%', height: '120px', backgroundColor: '#333'}}>
-        Map
+      <div style={{width: '100%', height: '320px'}}>
+        <GoogleMapReact
+           bootstrapURLKeys={{ key: ''}}
+           defaultCenter={getOfficeLocation('singapore')}
+           defaultZoom={zoom}
+           center={getOfficeLocation(office)}
+          >
+        </GoogleMapReact>
       </div>
 
       <div>
